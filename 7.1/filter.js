b42757a1499758960076aa9709c87d71f670f576
Victor Vo <79146455+victorkvo@users.noreply.github.com>
2021-03-29 15:47:26 -0500

    update to 7.1

--- 7.1/filter.js
+++ 7.1/filter.js
@@ -2,29 +2,64 @@
 
 
 
-
-
-
-
-
+function genreTitle() {
+  // e.preventDefault();
+  let genre = books.value;
 
 var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-  };
-  
-  fetch(`http://openlibrary.org/subjects/${subject}.json?`, requestOptions)
-    .then(response => response.json())
-    .then((result) => {
+  method: 'GET',
+  redirect: 'follow'
+};
 
-        console.log(result.docs[0].title);
+fetch(`http://openlibrary.org/subjects/${genre}.json?`, requestOptions)
+  .then(response => response.json())
+  .then((result) => {
+    for(let i=0; i<10; i++){
+    document.getElementById(`title${i}`).innerHTML = result.works[i].title;
+    var isbn = result.works[i].cover_id
+    document.getElementById(`book${i}`).src = `http://covers.openlibrary.org/b/id/${isbn}-L.jpg`;
+    var works = result.works[i].key;
+    return fetch(`https://openlibrary.org${works}.json`, requestOptions)
+    // .then(response => response.json())
+    // .then((result) => {
+    // console.log(result.description)
+    // document.getElementById(`desc${i}`).innerHTML = result.description.value;
+  }})
     
-        document.getElementById('book1').innerHTML = result.works[0].title;
-        document.getElementById('book2').innerHTML = result.works[1].title;
-        document.getElementById('book3').innerHTML = result.works[2].title;
-        document.getElementById('book4').innerHTML = result.works[3].title;
-        document.getElementById('book5').innerHTML = result.works[4].title;
-        document.getElementById('book6').innerHTML = result.works[5].title;
-      })
+  
+  
+  
+  .catch(error => console.log('error', error));
 
-    .catch(error => console.log('error', error));
+  }
+
+
+
+
+
+
+
+
+
+
+
+// var requestOptions = {
+//     method: 'GET',
+//     redirect: 'follow'
+//   };
+  
+//   fetch(`http://openlibrary.org/subjects/${subject}.json?`, requestOptions)
+//     .then(response => response.json())
+//     .then((result) => {
+
+//         console.log(result.docs[0].title);
+    
+//         document.getElementById('book1').innerHTML = result.works[0].title;
+//         document.getElementById('book2').innerHTML = result.works[1].title;
+//         document.getElementById('book3').innerHTML = result.works[2].title;
+//         document.getElementById('book4').innerHTML = result.works[3].title;
+//         document.getElementById('book5').innerHTML = result.works[4].title;
+//         document.getElementById('book6').innerHTML = result.works[5].title;
+//       })
+
+//     .catch(error => console.log('error', error));
--- 7.1/index.js
+++ 7.1/index.js
@@ -13,17 +13,19 @@ var requestOptions = {
 fetch(`http://openlibrary.org/search.json?author=&title=${title}`, requestOptions)
   .then(response => response.json())
   .then((result) => {
-    console.log(result.docs[0].title);
-    document.getElementById('titleP').innerHTML = result.docs[0].title;
-    var isbn = result.docs[0].isbn[0]
-    document.getElementById("book1").src = `http://covers.openlibrary.org/b/ISBN/${isbn}-L.jpg`;
-    var works = result.docs[0].key;
+    for(var i=0; i<10; i++){
+    document.getElementById(`title${i}`).innerHTML = result.docs[i].title;
+    var isbn = result.docs[i].isbn[0]
+    document.getElementById(`book${i}`).src = `http://covers.openlibrary.org/b/ISBN/${isbn}-L.jpg`;
+    var works = result.docs[i].key;
     return fetch(`https://openlibrary.org${works}.json`, requestOptions)
-  })
+  
   .then(response => response.json())
   .then((result) => {
     console.log(result.description)
-    document.getElementById('desc').innerHTML = result.description;
+    document.getElementById(`desc${i}`).innerHTML = result.description;
+  })
+};
   })
   .catch(error => console.log('error', error));
 
